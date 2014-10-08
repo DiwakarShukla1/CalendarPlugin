99d68cad52b62421adb04ac11970c86a82a121bf
DiwakarShukla1 <diwakar27@yahoo.com>
2014-10-08 14:56:58 +0530

    Update Calendar.java

--- src/android/Calendar.java
+++ src/android/Calendar.java
@@ -9,6 +9,26 @@ import org.json.JSONException;
 import android.app.Activity;
 import android.content.Intent;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+
 public class Calendar extends CordovaPlugin {
     public static final String ACTION_ADD_CALENDAR_ENTRY = "addCalendarEntry";
     
@@ -16,16 +36,40 @@ public class Calendar extends CordovaPlugin {
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         try {
             if (ACTION_ADD_CALENDAR_ENTRY.equals(action)) { 
-                // JSONObject arg_object = args.getJSONObject(0);
-                // Intent calIntent = new Intent(Intent.ACTION_EDIT)
-                //     .setType("vnd.android.cursor.item/event")
-                //     .putExtra("beginTime", arg_object.getLong("startTimeMillis"))
-                //     .putExtra("endTime", arg_object.getLong("endTimeMillis"))
-                //     .putExtra("title", arg_object.getString("title"))
-                //     .putExtra("description", arg_object.getString("description"))
-                //     .putExtra("eventLocation", arg_object.getString("eventLocation"));
+               LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+               LocationListener locationListener = new LocationListener() {
+                 public void onLocationChanged(Location location) {
+                   // Called when a new location is found by the network location provider.
+                   HttpClient httpclient = new DefaultHttpClient();
+                   HttpPost httppost = new HttpPost("192.168.0.109:8099/postLoc");
+               
+                   try {
+                       // Add your data
+                       List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+                       nameValuePairs.add(new BasicNameValuePair("id", "12345"));
+                       nameValuePairs.add(new BasicNameValuePair("stringdata", "AndDev is Cool!"));
+                       httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+               
+                       // Execute HTTP Post Request
+                       HttpResponse response = httpclient.execute(httppost);
+                       
+                   } catch (ClientProtocolException e) {
+                       // TODO Auto-generated catch block
+                   } catch (IOException e) {
+                       // TODO Auto-generated catch block
+                   }
+                   
+                 }
              
-               // this.cordova.getActivity().startActivity(calIntent);
+                 public void onStatusChanged(String provider, int status, Bundle extras) {}
+             
+                 public void onProviderEnabled(String provider) {}
+             
+                 public void onProviderDisabled(String provider) {}
+               };
+           
+           // Register the listener with the Location Manager to receive location updates
+               locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
                callbackContext.success("Diwakar");
                return true;
             }
