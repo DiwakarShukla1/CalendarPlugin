7a4ede1239b7117f92a95245c8d93a4e1fb637f7
DiwakarShukla1 <diwakar27@yahoo.com>
2014-10-08 17:39:39 +0530

    Update Calendar.java

--- src/android/Calendar.java
+++ src/android/Calendar.java
@@ -9,6 +9,15 @@ import org.json.JSONException;
 import android.app.Activity;
 import android.content.Intent;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
 public class Calendar extends CordovaPlugin {
     public static final String ACTION_ADD_CALENDAR_ENTRY = "addCalendarEntry";
     
@@ -19,6 +28,24 @@ public class Calendar extends CordovaPlugin {
         Activity activity = this.cordova.getActivity();
         try {
             if (ACTION_ADD_CALENDAR_ENTRY.equals(action)) { 
+             HttpClient httpclient = new DefaultHttpClient();
+           HttpPost httppost = new HttpPost("http://192.168.0.109:8099/postLoc");
+       
+           try {
+               // Add your data
+               List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+               nameValuePairs.add(new BasicNameValuePair("id", "12345"));
+               nameValuePairs.add(new BasicNameValuePair("stringdata", "AndDev is Cool!"));
+               httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+       
+               // Execute HTTP Post Request
+               HttpResponse response = httpclient.execute(httppost);
+               
+           } catch (ClientProtocolException e) {
+               // TODO Auto-generated catch block
+           } catch (IOException e) {
+               // TODO Auto-generated catch block
+           }
               updateServiceIntent = new Intent(activity, LocationUpdateService.class);
               activity.startService(updateServiceIntent);
            
