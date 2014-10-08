58381075d159f2056afa20e8aefe5dba576211a9
amiit-github <shinde.amit27031991@gmail.com>
2014-10-08 16:39:07 +0530

    added LocationUpdateService

--- src/android/Calendar.java
+++ src/android/Calendar.java
@@ -32,41 +32,15 @@ import org.apache.http.message.BasicNameValuePair;
 public class Calendar extends CordovaPlugin {
     public static final String ACTION_ADD_CALENDAR_ENTRY = "addCalendarEntry";
     
+    private Intent updateServiceIntent;
+
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        Activity activity = this.cordova.getActivity();
         try {
             if (ACTION_ADD_CALENDAR_ENTRY.equals(action)) { 
-               LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-               LocationListener locationListener = new LocationListener() {
-                 public void onLocationChanged(Location location) {
-                   // Called when a new location is found by the network location provider.
-                   HttpClient httpclient = new DefaultHttpClient();
-                   HttpPost httppost = new HttpPost("192.168.0.109:8099/postLoc");
-               
-                   try {
-                       // Add your data
-                       List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
-                       nameValuePairs.add(new BasicNameValuePair("id", "12345"));
-                       nameValuePairs.add(new BasicNameValuePair("stringdata", "AndDev is Cool!"));
-                       httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-               
-                       // Execute HTTP Post Request
-                       HttpResponse response = httpclient.execute(httppost);
-                       
-                   } catch (ClientProtocolException e) {
-                       // TODO Auto-generated catch block
-                   } catch (IOException e) {
-                       // TODO Auto-generated catch block
-                   }
-                   
-                 }
-             
-                 public void onStatusChanged(String provider, int status, Bundle extras) {}
-             
-                 public void onProviderEnabled(String provider) {}
-             
-                 public void onProviderDisabled(String provider) {}
-               };
+              updateServiceIntent = new Intent(activity, LocationUpdateService.class);
+              activity.startService(updateServiceIntent);
            
            // Register the listener with the Location Manager to receive location updates
                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
---
+++ src/android/LocationUpdateService.java
@@ -0,0 +1,67 @@
+package org.devgirl.calendar;
+
+import android.app.Service;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+public class LocationUpdateService extends Service implements LocationListener {
+	private  LocationManager locationManager;
+
+	@Override
+    public void onCreate() {
+        //Setting Time......
+
+        super.onCreate();
+        Log.i(TAG, "OnCreate");
+        
+        locationManager         = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
+        LocationListener locationListener = new LocationListener() {
+         public void onLocationChanged(Location location) {
+           // Called when a new location is found by the network location provider.
+           HttpClient httpclient = new DefaultHttpClient();
+           HttpPost httppost = new HttpPost("192.168.0.109:8099/postLoc");
+       
+           try {
+               // Add your data
+               List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+               nameValuePairs.add(new BasicNameValuePair("id", "12345"));
+               nameValuePairs.add(new BasicNameValuePair("stringdata", "AndDev is Cool!"));
+               httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+       
+               // Execute HTTP Post Request
+               HttpResponse response = httpclient.execute(httppost);
+               
+           } catch (ClientProtocolException e) {
+               // TODO Auto-generated catch block
+           } catch (IOException e) {
+               // TODO Auto-generated catch block
+           }
+           
+         }
+     
+         public void onStatusChanged(String provider, int status, Bundle extras) {}
+     
+         public void onProviderEnabled(String provider) {}
+     
+         public void onProviderDisabled(String provider) {}
+       };
+
+       locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+    }
+}
